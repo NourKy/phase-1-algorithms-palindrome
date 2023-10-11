db627081078e37fab5bf71c550335093dafb0cae
Nour kayyali <nourkayyali91@gmail.com>
2023-10-11 23:49:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const lowerWord=word.toLowerCase().replace(/[^a-z]/g, '');
+  for (let i=0;i<lowerWord.length/2;i++)
+  {
+    if (lowerWord[i]!==(lowerWord[lowerWord.length-1-i]))
+    {
+      return false;
+    }
+  
+
+  }
+  return true; 
 }
 
 /* 
-  Add your pseudocode here
+ isPalindrome, return true if it can be read the word the same forward and back, else return false
 */
 
 /*
-  Add written explanation of your solution here
+ lowerWord >> removed any char not in lowercase 
+ check by lenght
+  
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,7 +28,8 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
+  console.log("=>", isPalindrome("le2vel"));
+  console.log("=>", isPalindrome("Da d"));
   console.log("");
 
   console.log("Expecting: false");
